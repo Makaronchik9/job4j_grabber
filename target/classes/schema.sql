06b446078880a6f1a1e333695f87b8b768e8c962
Konstantin <89169555954@mail.ru>
2026-02-12 16:07:14 +0300

    агрегатор вакансии

--- target/classes/schema.sql
+++
@@ -1,7 +0,0 @@
-create table if not exists post (
-    id serial primary key,
-    name varchar(255) not null,
-    text text not null,
-    link varchar(255) not null unique,
-    created timestamp without time zone not null
-);
